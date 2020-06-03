144dbca88bbeb457d1da6314d872f6c85706e70b
Robin Kühling <robin.kuehling@gmail.com>
2020-06-03 13:24:28 +0200

    train_test_split methode

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -2,19 +2,22 @@ from tensorflow.keras.datasets import cifar10, mnist
 import json
 import os
 from pathlib import Path
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 def get_mnist() -> dict:
     if not os.path.isfile(Path('../../data/raw/mnist.json')):
         (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-        mnist_data = {'train_images':train_images,
-                    'train_labels':train_labels,
-                    'test_images':test_images,
-                    'test_labels':test_labels
+        
+        images = np.asarray(list(test_images)+list(train_images))
+        labels = np.asarray(list(test_labels)+list(train_labels))
+        
+        mnist_data = {'images':images,
+                    'labels':labels,
                     }
         with open('../../data/raw/mnist.json', 'w') as f:
             json.dump(mnist_data, f)
-    
-    
+            
     with open('../../data/raw/mnist.json') as f:
         mnist_data = json.load(f)
         
@@ -22,12 +25,15 @@ def get_mnist() -> dict:
 
 def get_cifar10() -> dict:
     if not os.path.isfile(Path('../../data/raw/cifar10.json')):
-        (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-        cifar10_data = {'train_images':train_images,
-                    'train_labels':train_labels,
-                    'test_images':test_images,
-                    'test_labels':test_labels
+        (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+        
+        images = np.asarray(list(test_images)+list(train_images))
+        labels = np.asarray(list(test_labels)+list(train_labels))
+        
+        cifar10_data = {'images':images,
+                        'labels':labels,
                     }
+        
         with open('../../data/raw/cifar10.json', 'w') as f:
             json.dump(cifar10_data, f)
             
@@ -44,3 +50,10 @@ def get_data(dataset: str) -> dict:
     else:
         raise ValueError('Desired dataset is not available. Please choose mnist or cifar10')
     
+
+def get_training_data(dataset: str, test_size: float) -> np.ndarray:
+    data = get_data(dataset)
+    
+    train_images, test_images, train_labels, test_labels = train_test_split(data['images'], data['labels'], test_size=test_size, random_state=42)
+    
+    return train_images, test_images, train_labels, test_labels
