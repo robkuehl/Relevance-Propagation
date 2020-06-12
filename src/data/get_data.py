9d999b113290e23a2443fdfcdae69a7003dce752
Robin Kühling <robin.kuehling@gmail.com>
2020-06-12 08:35:20 +0200

    _

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -1,74 +1,50 @@
-from tensorflow.keras.datasets import cifar10, mnist
+from tensorflow.keras.datasets import cifar10, mnist, fashion_mnist
 import pickle
 import os
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-def load_mnist():
+def load_keras_data(dataset: str):
     dirname = os.path.dirname(__file__)
     print(dirname)
-    filename = os.path.join(dirname, '../../data/raw/mnist.pickle')
+    filename = os.path.join(dirname, '../../data/raw/{}.pickle'.format(dataset))
     
     if not os.path.isfile(Path(filename)):
-        (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+        if dataset == "mnist":
+            (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+        elif dataset == "cifar10"
+            (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+        elif dataset == "fashion_mnist"
+            (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
         
         images = np.asarray(list(test_images)+list(train_images))
         labels = np.asarray(list(test_labels)+list(train_labels))
         
-        mnist_data = {'images':images,
+        data = {'images':images,
                     'labels':labels,
                     }
         with open(filename, 'wb') as f:
-            pickle.dump(mnist_data, f)
+            pickle.dump(data, f)
     else:
         pass
     
     return filename
-    
 
-def get_mnist() -> dict:
-    filename = load_mnist()
-    with open(filename, 'rb') as f:
-        mnist_data = pickle.load(f)
-        
-    return mnist_data
-    
 
-def load_cifar10():
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../../data/raw/mnist.pickle')
-    
-    if not os.path.isfile(Path(filename)):
-        (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
-        
-        images = np.asarray(list(test_images)+list(train_images))
-        labels = np.asarray(list(test_labels)+list(train_labels))
-        
-        cifar10_data = {'images':images,
-                        'labels':labels,
-                    }
-        
-        with open(filename, 'wb') as f:
-            pickle.dump(cifar10_data, f)
-    else:
-        pass
-    
-    return filename
-            
-def get_cifar10() -> dict:
-    filename = load_cifar10()
+def get_keras_data(dataset) -> dict:
+    filename = load_keras_data(dataset)
     with open(filename, 'rb') as f:
-        cifar10_data = pickle.load(f)
+        data = pickle.load(f)
         
-    return cifar10_data
+    return data
+
+
     
     
 def get_raw_data(dataset: str) -> dict:
-    if dataset == 'mnist':
-        return get_mnist()
-    elif dataset == 'cifar10':
-        return get_cifar10()
+    if dataset == 'mnist' or dataset == 'cifar10' or dataset == 'fasion_mnist':
+        return get_keras_data(dataset)
     else:
         raise ValueError('Desired dataset is not available. Please choose mnist or cifar10')
     
