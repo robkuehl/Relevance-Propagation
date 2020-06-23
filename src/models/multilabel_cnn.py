85f7568dd5fe31f8eadac37855adab2ce3e98dfd
Robin Kühling <robin.kuehling@gmail.com>
2020-06-23 11:53:53 +0200

    bugfixes zB Speichern von Bildern in eval_cnn

--- src/models/multilabel_cnn.py
+++ src/models/multilabel_cnn.py
@@ -72,8 +72,7 @@ class ml_cnn_classifier:
         self.train_images, self.validation_images, self.train_labels, self.validation_labels = train_test_split(self.train_images, self.train_labels, test_size=0.2)
         
         # create data generator
-        datagen = ImageDataGenerator(rescale=1.0/255.0,
-                                     rotation_range=50.0,
+        datagen = ImageDataGenerator(rotation_range=50.0,
                                      width_shift_range = 0.1,
                                      height_shift_range = 0.1,
                                      shear_range=0.1,
@@ -102,7 +101,7 @@ class ml_cnn_classifier:
     def pred(self, i):
         
         image = self.test_images[i]
-        prediction = self.model.predict(np.array([image/255]))
+        prediction = self.model.predict(np.asarray([image], dtype=np.float64))
         plt.imshow(Image.fromarray(np.uint8(image)))
         plt.show()
         print('Correct Label: {}\n'.format(self.classes[np.argmax(self.test_labels[i])]))
@@ -122,7 +121,7 @@ class ml_cnn_classifier:
             image_name = self.test_labels_df.index[i]
             image = self.test_images[i]
             #model prediction
-            prediction = self.model.predict(np.asarray([image/255]))[0]
+            prediction = self.model.predict(np.asarray([image], dtype=np.float64))[0]
             correct_labels = [self.classes[j] for j in range(self.test_labels[0].shape[0]) if self.test_labels[i][j]==1]
             image_result = [image_name, correct_labels]+[prediction[i] for i in range(prediction.shape[0])]
             eval_df = eval_df.append(pd.DataFrame([image_result], columns=eval_df.columns), ignore_index=True)
