5e72bce6b2c5bb7a4391414352cdc843bdabd57f
Robin Kühling <robin.kuehling@gmail.com>
2020-08-02 19:39:57 +0200

    aufräumen

--- src/models/help_functions/cnn_models.py
+++ src/models/help_functions/cnn_models.py
@@ -51,7 +51,7 @@ def get_model(model_name: str, input_shape: tuple, output_shape: int, final_acti
         model.add(Dense(output_shape))
         model.add(Activation(final_activation))
         
-    
+    # TODO: Delete
     if model_name == 'vgg16':
         vgg16_model = VGG16()
         model = Sequential()
